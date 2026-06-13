805d016d96ebe9dd112e333c09210021bffd2767
Eduard Dopler <kontakt@eduard-dopler.de>
2026-06-13 21:01:10 +0200

    Configure dynamic base path in vite to support GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
+// Ensure base path starts and ends with a slash, and contains no duplicate slashes
+let base = process.env.VITE_PUBLIC_PATH || '/';
+if (!base.startsWith('/')) {
+  base = '/' + base;
+}
+if (!base.endsWith('/')) {
+  base = base + '/';
+}
+base = base.replace(/\/+/g, '/');
+
 export default defineConfig({
+  base,
   test: {
     environment: 'node',
     globals: true,
-    include: ['src/tests/**/*.test.ts']
+    include: ['src/tests/**/*.test.ts'],
   }
 });
